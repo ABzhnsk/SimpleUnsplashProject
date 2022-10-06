df5361a603a61f4f19340e149f1a69b375eb04e6
ABzhnsk <96691110+ABzhnsk@users.noreply.github.com>
2022-10-06 18:16:09 +0300

    Add Search View logic

---
+++ SimpleUnsplashProject/SimpleUnsplashProject/View/Search/SearchView.swift
@@ -0,0 +1,43 @@
+//
+//  SearchView.swift
+//  SimpleUnsplashProject
+//
+//  Created by Anna Buzhinskaya on 06.10.2022.
+//
+
+import UIKit
+
+final class SearchView: UIView {
+    private(set) var collectionView: UICollectionView = {
+        let viewLayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: viewLayout)
+        collectionView.backgroundColor = .white
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    private(set) var searchBar = UISearchBar()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+        addSubviews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubviews() {
+        [collectionView].forEach { subview in addSubview(subview) }
+    }
+    private func setupConstraints() {
+        let constraints = [
+            collectionView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leftAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: self.safeAreaLayoutGuide.rightAnchor)
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
+}
--- SimpleUnsplashProject/SimpleUnsplashProject/View/Search/SearchViewController.swift
+++ SimpleUnsplashProject/SimpleUnsplashProject/View/Search/SearchViewController.swift
@@ -11,43 +11,36 @@ class SearchViewController: UIViewController {
     private enum LayoutConstant {
         static let spacing: CGFloat = 8.0
     }
-    private let collectionView: UICollectionView = {
-        let viewLayout = UICollectionViewFlowLayout()
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: viewLayout)
-        collectionView.backgroundColor = .white
-        return collectionView
-    }()
+    private let searchView = SearchView()
 
+    override func loadView() {
+        view = searchView
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
-        setupConstraints()
+        configureCollection()
+        setupNavBar()
     }
 }
 
+// MARK: - Configure collection and setup navigation bar
 extension SearchViewController {
-    private func setupUI() {
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.register(SearchCollectionViewCell.self, forCellWithReuseIdentifier: SearchCollectionViewCell.identifier)
-        self.view = collectionView
+    private func configureCollection() {
+        searchView.collectionView.delegate = self
+        searchView.collectionView.dataSource = self
+        searchView.collectionView.register(SearchCollectionViewCell.self, forCellWithReuseIdentifier: SearchCollectionViewCell.identifier)
     }
-    private func setupConstraints() {
-        let constraints = [
-            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            collectionView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
-            collectionView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor)
-        ]
-        NSLayoutConstraint.activate(constraints)
+    private func setupNavBar() {
+        navigationItem.titleView = searchView.searchBar
+        searchView.searchBar.delegate = self
     }
 }
 
+// MARK: - UICollectionViewDataSource
 extension SearchViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 9
     }
-    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchCollectionViewCell.identifier, for: indexPath) as? SearchCollectionViewCell else { return UICollectionViewCell() }
         cell.config(image: UIImage(systemName: "house"))
@@ -55,28 +48,35 @@ extension SearchViewController: UICollectionViewDataSource {
     }
 }
 
+// MARK: - UICollectionViewDelegateFlowLayout
 extension SearchViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = itemWidth(for: view.frame.width, spacing: LayoutConstant.spacing)
         return CGSize(width: width, height: width)
     }
-    
     func itemWidth(for width: CGFloat, spacing: CGFloat) -> CGFloat {
         let itemsInRow: CGFloat = 2
         let totalSpacing: CGFloat = 2 * spacing + (itemsInRow - 1) * spacing
         let finalWidth = (width - totalSpacing) / itemsInRow
         return floor(finalWidth)
     }
-    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: LayoutConstant.spacing, left: LayoutConstant.spacing, bottom: LayoutConstant.spacing, right: LayoutConstant.spacing)
     }
-    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return LayoutConstant.spacing
     }
-    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return LayoutConstant.spacing
+    }
+}
+
+// MARK: - UISearchBarDelegate
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        searchView.searchBar.endEditing(true)
     }
 }
